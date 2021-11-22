3fcf4b4c1297ec1ef34558c63df6d2a80a9d513f
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2021-11-21 23:12:42 -0300

    Projeto basico CRUD Fatec 2022

---
+++ fatec2022/src/main/java/br/edu/fatec2022/Fatec2022Application.java
@@ -0,0 +1,13 @@
+package br.edu.fatec2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fatec2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fatec2022Application.class, args);
+	}
+
+}
---
+++ fatec2022/src/test/java/br/edu/fatec2022/Fatec2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatec2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fatec2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
