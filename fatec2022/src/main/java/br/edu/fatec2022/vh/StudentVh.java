9061789878cc2496cea518442a3b4642e1f59a36
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-10-02 23:38:19 -0300

    Melhoria na response da ViewHelper, verificando tipo da response.

--- fatec2022/src/main/java/br/edu/fatec2022/vh/StudentVh.java
+++ fatec2022/src/main/java/br/edu/fatec2022/vh/StudentVh.java
@@ -2,7 +2,9 @@ package br.edu.fatec2022.vh;
 
 import java.time.LocalDate;
 
+import br.edu.fatec2022.entity.Message;
 import br.edu.fatec2022.entity.Student;
+import br.edu.fatec2022.utils.ParametersUtils;
 import br.edu.fatec2022.vo.EntityVo;
 
 public class StudentVh implements EntityVh {
@@ -10,33 +12,38 @@ public class StudentVh implements EntityVh {
 	public Student getEntityRequest(EntityVo e) {
 		return Student.builder()
 				.id(e.getId() != null ? e.getId() : null)
-				.firstName(e.getFirstName() != null ? e.getFirstName() : "")
-				.mediumName(e.getMediumName() != null ? e.getMediumName() : "")
-				.lastName(e.getLastName() != null ? e.getLastName() : "")
-				.birthDate(e.getBirthDate() != null && !e.getBirthDate().equals("") ? this.getDate(e.getBirthDate()) : null)
-				.startDate(e.getStartDate() != null && !e.getStartDate().equals("") ? this.getDate(e.getStartDate()) : null)
-				.endDate(e.getEndDate() != null && !e.getEndDate().equals("") ? this.getDate(e.getEndDate()) : null)
-				.enrollNumber(e.getEnrollNumber() != null ? e.getEnrollNumber() : "")
+				.firstName(e.getFirstName() != null ? e.getFirstName() : ParametersUtils.EMPTY)
+				.mediumName(e.getMediumName() != null ? e.getMediumName() : ParametersUtils.EMPTY)
+				.lastName(e.getLastName() != null ? e.getLastName() : ParametersUtils.EMPTY)
+				.birthDate(e.getBirthDate() != null && !e.getBirthDate().equals(ParametersUtils.EMPTY) ? this.getDate(e.getBirthDate()) : null)
+				.startDate(e.getStartDate() != null && !e.getStartDate().equals(ParametersUtils.EMPTY) ? this.getDate(e.getStartDate()) : null)
+				.endDate(e.getEndDate() != null && !e.getEndDate().equals(ParametersUtils.EMPTY) ? this.getDate(e.getEndDate()) : null)
+				.enrollNumber(e.getEnrollNumber() != null ? e.getEnrollNumber() : ParametersUtils.EMPTY)
 				.position(e.getPosition() != null ? e.getPosition() : null)
-				.email(e.getEmail() != null ? e.getEmail() : "")
+				.email(e.getEmail() != null ? e.getEmail() : ParametersUtils.EMPTY)
 				.build();
 	}
 
 	@Override
 	public EntityVo getEntityResponse(Object obj) {
-		Student student = (Student)obj;
-		return EntityVo.builder()
-				.id(student.getId())
-				.firstName(student.getFirstName())
-				.mediumName(student.getMediumName())
-				.lastName(student.getLastName())
-				.birthDate(String.valueOf(student.getBirthDate()))
-				.startDate(String.valueOf(student.getStartDate()))
-				.endDate(student.getEndDate() != null ? String.valueOf(student.getEndDate()) : "")
-				.enrollNumber(student.getEnrollNumber())
-				.position(student.getPosition())
-				.email(student.getEmail())
-				.build();
+		if(obj instanceof Student) {
+			Student student = (Student)obj;
+			return EntityVo.builder()
+					.id(student.getId())
+					.firstName(student.getFirstName())
+					.mediumName(student.getMediumName())
+					.lastName(student.getLastName())
+					.birthDate(String.valueOf(student.getBirthDate()))
+					.startDate(String.valueOf(student.getStartDate()))
+					.endDate(student.getEndDate() != null ? String.valueOf(student.getEndDate()) : ParametersUtils.EMPTY)
+					.enrollNumber(student.getEnrollNumber())
+					.position(student.getPosition())
+					.email(student.getEmail())
+					.build();
+		} else {
+			Message msg = (Message)obj;
+			return EntityVo.builder().msg(msg.getValue()).build();
+		}
 	}
 
 	private LocalDate getDate(String date) {
