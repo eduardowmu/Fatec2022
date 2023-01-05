61654cdddd77a9c5d339e2de7d2a606dff8a8466
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-01-05 00:23:41 -0300

    Projeto frontend do crud da fatec de mogi das cruzes

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  //isso irá buscar o arquivo HTML para carregar os componentes ali dentro.
+  templateUrl: 'app.component.html',
+})
+
+export class AppComponent {
+  title = 'Fatec';
+}
